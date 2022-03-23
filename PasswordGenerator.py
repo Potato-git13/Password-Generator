b2618cfd194d4fca4066628e561124c362bacf36
potato <lanveble08@gmail.com>
2022-03-23 19:18:57 +0100

    Refactored and changed a bit & remove PassordGeneratorwSeed

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,9 +1,11 @@
-# importing a module
 import random
+import time
 
+def random_string(string_length, must_start):
+    # define the password length
+    len_start = len(must_start)
+    string_length = string_length - len_start
 
-def random_string(string_length=8):
-    # creating a list of characters used
     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
                'V', 'W', 'X', 'Y', 'Z',
                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -11,18 +13,23 @@ def random_string(string_length=8):
                '1', '2', '3', '4', '5', '6', '7', '8', '9',
                '!', '"', '#', '$', '%', '&', '/', '(', ')', '=', '?', '*', ',', '.', '-', ';', ':', '_']
     # returning the result
-    return ''.join(random.choice(letters) for i in range(string_length))
+    wout_start = ''.join(random.choice(letters) for i in range(string_length))
+    out = must_start + wout_start
+    return out
 
+# collecting the input
+length = int(input("length >"))
+must_start_inp = input("must start with >")
+pass_amount = int(input("amount of passwords >"))
+# creating the filename
+time = time.strftime("%Y-%m-%d-%H:%M:%S")
+filename = f"passwords-{time}"
 
-rand_num = random.randint(0, 99999)
-# opening a new file
-f = open(f"seed {rand_num}.txt", "x")
-i = 0
-# collecting the input 
-length = int(input(">"))
-# creating 999,999 random passwords
-while i < 999999:
-    print(f"{i}")
-    # writing in the file
-    f.write(f"{random_string(length)} + {i}\n")
-    i += 1
+with open(filename, "w") as f:
+    counter = 0
+
+    while counter < pass_amount:
+        print(f"{counter}")
+        # writing to the file
+        f.write(f"{random_string(length, must_start_inp)}\n")
+        counter += 1
